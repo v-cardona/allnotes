b8d4ea8e9cf70024755d07158f01489608e0ade6
Victor Cardona <vicardona7@gmail.com>
2021-01-27 20:49:09 +0100

    add show passowrd on password inputs

--- lib/presentation/widgets/password_input.dart
+++ lib/presentation/widgets/password_input.dart
@@ -2,23 +2,33 @@ import 'package:allnotes/common/constants/size_constants.dart';
 import 'package:allnotes/common/extensions/size_extensions.dart';
 import 'package:flutter/material.dart';
 
-class PasswordInput extends StatelessWidget {
+class PasswordInput extends StatefulWidget {
   final Function onChanged;
   final String labelText;
   final String errorText;
 
   const PasswordInput(
-      {Key key, @required this.onChanged, @required this.errorText, @required this.labelText})
+      {Key key,
+      @required this.onChanged,
+      @required this.errorText,
+      @required this.labelText})
       : super(key: key);
+
+  @override
+  _PasswordInputState createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  bool _isPasswordHidden = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: Sizes.dimen_20.w),
       child: TextField(
-        onChanged: onChanged,
+        onChanged: widget.onChanged,
         keyboardType: TextInputType.visiblePassword,
-        obscureText: true,
+        obscureText: _isPasswordHidden,
         enableSuggestions: false,
         autocorrect: false,
         decoration: InputDecoration(
@@ -26,9 +36,20 @@ class PasswordInput extends StatelessWidget {
               Icons.lock_outline,
               color: Colors.deepPurple,
             ),
-            labelText: labelText,
-            errorText: errorText),
+            labelText: widget.labelText,
+            errorText: widget.errorText,
+            suffixIcon: IconButton(
+              icon: Icon(_isPasswordHidden ? Icons.visibility : Icons.visibility_off, color: Colors.grey,),
+              onPressed: _togglePasswordView,
+            )),
       ),
     );
   }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isPasswordHidden = !_isPasswordHidden;
+    });
+  }
+
 }
